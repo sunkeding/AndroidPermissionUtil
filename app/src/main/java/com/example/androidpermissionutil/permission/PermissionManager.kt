9e3200571f0452c16fd74317a9abac8a1eeb48ec
sunkeding <970335012@qq.com>
2020-07-19 09:39:28 +0800

    权限申请Activity跳转前判断线程情况，保证页面跳转始终在主线程

--- app/src/main/java/com/example/androidpermissionutil/permission/PermissionManager.kt
+++ app/src/main/java/com/example/androidpermissionutil/permission/PermissionManager.kt
@@ -3,6 +3,8 @@ package com.example.androidpermissionutil.permission
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.support.v4.content.ContextCompat
 
 /**
@@ -10,11 +12,25 @@ import android.support.v4.content.ContextCompat
  */
 class PermissionManager {
     companion object {
+        private val mainHandler = Handler(Looper.getMainLooper())
         fun requestPermission(
             context: Context,
             permissions: Array<String?>?,
             iPermissionResultCallback: IPermissionResultCallback?
         ) {
+            if (Looper.myLooper() == Looper.getMainLooper()){
+                requestPermissionInternal(context, permissions, iPermissionResultCallback)
+            }else{
+                mainHandler.post {
+                    requestPermissionInternal(context, permissions, iPermissionResultCallback)
+                }
+            }
+        }
+        fun requestPermissionInternal(
+            context: Context,
+            permissions: Array<String?>?,
+            iPermissionResultCallback: IPermissionResultCallback?
+        ){
             PermissionActivity.requestPermissionAction(
                 context,
                 permissions,
